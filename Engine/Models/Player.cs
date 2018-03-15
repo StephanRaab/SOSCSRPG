27f580756f5532258a92ef695a8b3aa40700c074
StephanRaab <stephan.raab@nyhart.com>
2018-03-15 16:12:24 -0400

    Player Class

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: stephan.raab
+ * Date: 3/15/18
+ * Time: 12:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Engine.Models
+{
+	/// <summary>
+	/// Description of Player.
+	/// </summary>
+	public class Player
+	{
+		string Name { get; set; }
+		string CharacterClass { get; set; }
+		int HitPoints { get; set; }
+		int ExperiencePoints { get; set; }
+		int Level { get; set; }
+		int Gold { get; set; }
+	}
+}
