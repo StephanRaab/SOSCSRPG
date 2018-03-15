c46fd179ee63093ae6163d11ca4a1650b3175a2b
StephanRaab <stephan.raab@nyhart.com>
2018-03-15 16:26:19 -0400

    Added GameSession as the first ViewModel

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,25 +1,14 @@
-﻿/*
- * Created by SharpDevelop.
- * User: stephan.raab
- * Date: 3/15/18
- * Time: 12:43
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 
 namespace Engine.Models
 {
-	/// <summary>
-	/// Description of Player.
-	/// </summary>
 	public class Player
 	{
-		string Name { get; set; }
-		string CharacterClass { get; set; }
-		int HitPoints { get; set; }
-		int ExperiencePoints { get; set; }
-		int Level { get; set; }
-		int Gold { get; set; }
+		public string Name { get; set; }
+		public string CharacterClass { get; set; }
+		public int HitPoints { get; set; }
+		public int ExperiencePoints { get; set; }
+		public int Level { get; set; }
+		public int Gold { get; set; }
 	}
 }
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+	public class GameSession
+	{
+		Player CurrentPlayer {get; set;}
+		
+		public GameSession()
+		{
+			// This is the constructor
+			CurrentPlayer = new Player();
+			CurrentPlayer.Name = "Jovan";
+			CurrentPlayer.Gold = 1000000;
+		}
+	}
+}
