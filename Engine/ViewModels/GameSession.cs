1e9d60c888ed57283cece7ada237b4ab1a5919ba
StephanRaab <stephan.raab@nyhart.com>
2018-03-16 15:57:55 -0400

    Creating the Location Class

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -6,6 +6,7 @@ namespace Engine.ViewModels
 	public class GameSession
 	{
 		public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
 		public GameSession()
 		{
@@ -17,6 +18,13 @@ namespace Engine.ViewModels
 			CurrentPlayer.Gold = 1000000;
 			CurrentPlayer.ExperiencePoints = 0;
 			CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Models.Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house.";
+            CurrentLocation.ImageName = "/Engine;Component/Images/Locations/Home.png";
 		}
 	}
 }
